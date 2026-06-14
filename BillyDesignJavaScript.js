64ee30c116cf8fd39e830de67a15bc5ba0ca3571
BillyTran.Design <95834823+BillyTranDesign@users.noreply.github.com>
2026-06-14 01:23:50 -0700

    Add files via upload

--- BillyDesignJavaScript.js
+++ BillyDesignJavaScript.js
@@ -1,11 +1,10 @@
-// JavaScript Document/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-// Top Navigator JS */
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+// JavaScript Document
+
+/* Mobile nav: the hamburger menu is now handled by pure CSS (a hidden
+   checkbox + label in the HTML), so no JavaScript toggle is needed here.
+   The old #myTopnav "responsive" toggle was removed because it ran on
+   page load and forced the mobile menu open. */
+
 	// This is for the type carousel//
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
@@ -16,24 +15,18 @@ var TxtRotate = function(el, toRotate, period) {
   this.tick();
   this.isDeleting = false;
 };
-
 TxtRotate.prototype.tick = function() {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
-
   if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
   var that = this;
   var delta = 300 - Math.random() * 100;
-
   if (this.isDeleting) { delta /= 2; }
-
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
@@ -42,16 +35,12 @@ TxtRotate.prototype.tick = function() {
     this.loopNum++;
     delta = 500;
   }
-
   setTimeout(function() {
     that.tick();
   }, delta);
 };
-
-
-
 window.onload = function() {
-	// Code 1 - text carousel 
+	// Code 1 - text carousel
   var elements = document.getElementsByClassName('txt-rotate');
   for (var i=0; i<elements.length; i++) {
     var toRotate = elements[i].getAttribute('data-rotate');
@@ -65,27 +54,21 @@ window.onload = function() {
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
-
-
 };
 
-    
-  
+
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
-
 //  scroll in text fade
 // Detect request animation frame
 var scroll = window.requestAnimationFrame ||
              // IE Fallback
              function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
-
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
 function loop() {
-
     Array.prototype.forEach.call(elementsToShow, function(element){
       if (isElementInViewport(element)) {
         element.classList.add('is-visible');
@@ -93,13 +76,10 @@ function loop() {
         element.classList.remove('is-visible');
       }
     });
-
     scroll(loop);
 }
-
 // Call the loop for the first time
 loop();
-
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
   // special bonus for those using jQuery
@@ -118,22 +98,17 @@ function isElementInViewport(el) {
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
-
 	// This is for the fade in on scroll
 'use strict';
-
 var animatedElements = document.querySelectorAll('.animated');
-
 var debounce = function debounce(func) {
   var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 20;
   var immediate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
-
   var timeout = void 0;
   return function () {
     for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
-
     var context = undefined;
     var later = function later() {
       timeout = null;
@@ -145,14 +120,10 @@ var debounce = function debounce(func) {
     if (callNow) func.apply(context, args);
   };
 };
-
 var checkAnimation = function checkAnimation(e) { //Check if a scroll animated element is in view
-
   animatedElements.forEach(function (animatedElement) {
     var slideInTrigger = window.scrollY + window.innerHeight - animatedElement.offsetHeight;
-
     var isShowing = slideInTrigger > animatedElement.offsetTop;
-
     if (isShowing) {
       animatedElement.classList.remove('inactive'); //Unhide the element
     } else {
@@ -160,30 +131,22 @@ var checkAnimation = function checkAnimation(e) { //Check if a scroll animated e
     }
   });
 };
-
 var hideAnimated = function hideAnimated(e) {
   animatedElements.forEach( function(animatedElement) {
     animatedElement.classList.add('inactive'); //Hide elements by default
   })
 }
-
-window.addEventListener('scroll', debounce(checkAnimation, 20)); 
-
+window.addEventListener('scroll', debounce(checkAnimation, 20));
 // test for fade in out scroll image
-
 var fadeElements = document.getElementsByClassName('scrollFade');
-
 function scrollFade() {
   var viewportBottom = window.scrollY + window.innerHeight;
-
   for (var index = 0; index < fadeElements.length; index++) {
     var element = fadeElements[index];
     var rect = element.getBoundingClientRect();
-
     var elementFourth = rect.height/4;
     var fadeInPoint = window.innerHeight - elementFourth;
     var fadeOutPoint = -(rect.height/2);
-
     if (rect.top <= fadeInPoint) {
       element.classList.add('scrollFade--visible');
       element.classList.add('scrollFade--animate');
@@ -192,14 +155,12 @@ function scrollFade() {
       element.classList.remove('scrollFade--visible');
       element.classList.add('scrollFade--hidden');
     }
-
     if (rect.top <= fadeOutPoint) {
       element.classList.remove('scrollFade--visible');
       element.classList.add('scrollFade--hidden');
     }
   }
 }
-
 document.addEventListener('scroll', scrollFade);
 window.addEventListener('resize', scrollFade);
 document.addEventListener('DOMContentLoaded', function() {
